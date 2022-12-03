dd6a712c95c0d43b005e4a9b6bccfdf245703865
Vedic Panda <vedic.panda@gmail.com>
2022-12-03 15:15:04 -0500

    Started process of connecting the frontend to the backend

--- backend/src/application.py
+++ backend/src/application.py
@@ -11,6 +11,12 @@ import ast
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+from scipy.spatial import distance
+from collections import OrderedDict
+import geopy.distance
+import math
+from operator import itemgetter
+
 # Initialize Flask App
 application = Flask(__name__, template_folder='html')
 application.config['SECRET_KEY'] = 'secret'
@@ -68,6 +74,25 @@ def get_recommendations():
     prefs = doc.get("prefs")
 
     print(living_history, prefs) # here's your data srajan & vedic :)
+
+    city_name = ""
+    
+    recommendations = cosine_distance_calculator("", living_history, city_name)
+
+    if city_name != "":
+        print("Here are top 5 county recommendations for you to relocate to along with their distance from your desired location!\n")
+    else:
+        print("Here are top 5 county recommendations for you to relocate to\n")
+    
+    for recommendation in recommendations:
+        if city_name != "":
+            print("County: " + str(recommendation[3]) + "\nState: " + str(recommendation[2]) + "\nDistance: " + str(recommendation[0]))
+        else:
+            print("County: " + str(recommendation[2]) + "\nState: " + str(recommendation[1]))
+        
+        print("\n")
+    
+    #to be added
     recs = {}
     return jsonify(recs)
 
@@ -126,6 +151,109 @@ def get_counties():
     counties = states_counties_dict[state]
     return jsonify(ast.literal_eval(counties))
 
+def cosine_distance_calculator(weights, history, city_name ='', max_distance = 50):
+
+    dataset = pd.read_csv("data.csv")
+
+    avg = []
+
+    for state, county in history:
+        row = []
+        row = dataset.loc[dataset['State'] == state]
+        row = row.loc[row['Area_name'] == county]
+        row = row.drop("State", axis = 1)
+        row = row.drop("Area_name", axis = 1)
+        
+        row = np.array(row)[0]
+
+        for i in range(len(row)):
+            if type(row[i]) == str:
+                if "," in row[i]:
+                    row[i] = int(row[i].replace(",", ""))
+            else: 
+                row[i] = int(row[i])
+
+        row = row[1:]
+
+        if len(avg) == 0:
+            avg = row
+        else:
+            avg = (avg + np.array(row)) / 2
+
+    dist_list = {}
+
+    for index, row in dataset.iterrows():
+        state = row['State']
+        county = row['Area_name']
+        row = row.drop("State")
+        row = row.drop("Area_name")
+
+        for i in range(len(row)):
+            if type(row[i]) == str:
+                if "," in row[i]:
+                    row[i] = int(row[i].replace(",", ""))
+                else:
+                    row[i] = int(row[i])
+            else:
+                row[i] = round(row[i])
+
+        row = row[1:]
+
+        dist_list[distance.cosine(avg, row)] = [state, county]
+
+
+    sorted_dictionary = OrderedDict(sorted(dist_list.items()))
+
+    result = np.array(list(sorted_dictionary.items()), dtype = object)
+
+    indices = np.where(result[:, 0] < 0.01)
+
+    good_matches = result[indices[0]]
+    
+    latlong = pd.read_csv("CountyAndLocation.csv")
+
+    newList = []
+    
+    for value, location in good_matches:
+        State = location[0]
+        County = location[1].split(" ")[0]
+
+        result = latlong.loc[latlong['State'] == State]
+        result = result.loc[latlong['Area_name'] == County]
+
+        latitude = result['Latitude'].tolist()
+        longitude = result['Longitude'].tolist()
+
+        if len(latitude) != 0 and len(longitude) != 0:
+            latitude = latitude[0][1:-1]
+            longitude = longitude[0][1:-1]
+
+        newList.append([value, State, location[1], latitude, longitude])
+
+    new_coord = ()
+
+    final_output = []
+
+    if city_name != '':
+
+        cities = pd.read_csv("majorcities.csv")
+        row = cities.loc[cities['City'] == city_name]
+        city_location = row["Location"].tolist()[0]
+        city_location = city_location.split(" ")
+        new_coord = (float(city_location[0][0:-2]), float(city_location[1][0:-2]))
+
+        for stuff in newList:
+            coords_1 = (stuff[3], stuff[4])
+            dist = geopy.distance.geodesic(coords_1, new_coord).mi
+            final_output.append([dist, stuff[0], stuff[1], stuff[2]])
+            
+        final_output = sorted(final_output, key=itemgetter(0))
+        final_output = sorted(final_output[:5], key=itemgetter(1))
+
+    else:
+        final_output = newList[0:5]
+
+    return final_output
 
 if __name__ == "__main__":
     application.run(host='127.0.0.1', port=5000, debug=False)
